611fce51fd976d431fa08d55f018d6c80c2cc919
Abhishek-Thakur <abhuthakur5567@gmail.com>
2025-02-19 10:34:25 +0530

    Create 83. Remove duplicates from sorted list.cpp

---
+++ 83. Remove duplicates from sorted list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
+
+output :-
+
+TEST CASE 1: - [1,2]
+TEST CASE 2: - [1,2,3]
