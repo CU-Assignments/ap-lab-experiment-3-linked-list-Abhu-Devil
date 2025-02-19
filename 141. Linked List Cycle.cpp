02f96fe852afb6db26dca3dfff780c7d4151c5c2
Abhishek-Thakur <abhuthakur5567@gmail.com>
2025-02-19 10:48:02 +0530

    Create 141. Linked List Cycle.cpp

---
+++ 141. Linked List Cycle.cpp
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+OUTPUT :-
+
+TEST CASE 1:-true
+TEST CASE 2:- TRUE
+TEST CASE 3:- false
